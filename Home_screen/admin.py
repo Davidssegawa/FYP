556761813c9b3e57348b9e23bfdc0ebb868607f9
unknown <ssegawajoe8@gmail.com>
2024-03-27 20:28:10 +0300

    testing

--- Home_screen/admin.py
+++ Home_screen/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import Meter_Address 
+from .models import Meter_Address,Meter_data 
 # Register your models here.
 admin.site.register(Meter_Address)
+admin.site.register(Meter_data)
