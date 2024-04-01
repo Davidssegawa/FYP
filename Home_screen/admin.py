7c4dfa947cbb6aa4faa4afe11f7135fb715b2009
unknown <ssegawajoe8@gmail.com>
2024-04-01 15:14:22 +0300

    waterunit model addition

--- Home_screen/admin.py
+++ Home_screen/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from .models import Meter_Address,Meter_data 
+from .models import Meter_Address,Meter_data,WaterUnit 
 # Register your models here.
 admin.site.register(Meter_Address)
 admin.site.register(Meter_data)
+admin.site.register(WaterUnit)
