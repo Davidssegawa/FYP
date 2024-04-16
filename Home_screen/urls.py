7afc1cda35905410f728e94bbe18fff0088b0abd
unknown <ssegawajoe8@gmail.com>
2024-04-16 12:06:56 +0300

    transaction_handler

--- Home_screen/urls.py
+++ Home_screen/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from . import views
 from .views import AddressView
-from .views import MeterDataList,PrepaymentOptionList, TransactionList
+from .views import MeterDataList,PrepaymentOptionList
 
 urlpatterns =[
     path('',views.index,name='index'),
