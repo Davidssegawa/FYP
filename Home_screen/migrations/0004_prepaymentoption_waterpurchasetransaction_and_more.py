4ff6977b72f63b0e4f3cb2b114e3aa662843c498
unknown <ssegawajoe8@gmail.com>
2024-04-24 10:35:33 +0300

    Transaction2_2

--- Home_screen/migrations/0004_prepaymentoption_waterpurchasetransaction_and_more.py
+++ Home_screen/migrations/0004_prepaymentoption_waterpurchasetransaction_and_more.py
@@ -12,6 +12,14 @@ class Migration(migrations.Migration):
 
     operations = [
         migrations.CreateModel(
+            name='PrepaymentOption',
+            fields=[
+                ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('name', models.CharField(max_length=100)),
+                ('price', models.DecimalField(decimal_places=2, max_digits=10)),
+            ],
+        ),
+        migrations.CreateModel(
             name='WaterPurchaseTransaction',
             fields=[
                 ('id', models.BigAutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
