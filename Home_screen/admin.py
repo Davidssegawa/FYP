4b09c4fb6bbb29d8f7bbe396a03ee4ca5e59a864
unknown <ssegawajoe8@gmail.com>
2024-04-24 11:13:09 +0300

    transaction

--- Home_screen/admin.py
+++ Home_screen/admin.py
@@ -1,7 +1,7 @@
 from django.contrib import admin
-from .models import Meter_Address,Meter_data,PrepaymentOption #,Transaction 
+from .models import Meter_Address,Meter_data,PrepaymentOption ,WaterPurchaseTransaction 
 # Register your models here.
 admin.site.register(Meter_Address)
 admin.site.register(Meter_data)
 admin.site.register(PrepaymentOption)
-#admin.site.register(Transaction)
+admin.site.register(WaterPurchaseTransaction)
